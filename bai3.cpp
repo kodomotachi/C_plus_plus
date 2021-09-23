e1495ca505f646cb3e145c994db4f675df037302
Tran Quoc Huy <62163667+kodomotachi@users.noreply.github.com>
2021-09-23 20:05:07 +0700

    Add files via upload

--- bai3.cpp
+++ bai3.cpp
@@ -2,38 +2,25 @@
 
 using namespace std;
 
-bool check(int n)
-{
-	for (int i = 2; i <= round(sqrt(n)); i++)
-	{
-		if (n%i == 0)
-			return false;
-	}
-	return true;
-}
+const int N = 1e5;
 
 int main()
 {
 	cin.tie(0)->sync_with_stdio(0);
-	int t;
-	cin >> t;
-	for (int i = 0; i < t; i++)
+	string s = "";
+	vector<int> arr[N];
+	while (s != "END")
 	{
-		int n;
-		cin >> n;
-		if (check(n))
-			return cout << 1 << " " << n - 1 << "\n";
-		else
+		cin >> s;
+		if (s == "NEW")
 		{
-			int min = 0;
-			for (int i = 2; i <= floor(n/2); i++)
-			{
-				if (n%i == 0)
-				{
-					min = i;
-					break;
-				}
-			}
+			int i, j;
+			cin >> i >> j;
+			arr[i].push_back(j);
+			arr[j].push_back(i);
+		}
+		else if (s == "ADD")
+		{
 			
 		}
 	}
--- test.cpp
+++ test.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main()
 {
-	cin.tie(0)->sync_with_stdio(0);
-	int a, b;
-	cin >> a >> b;
-	cout << max(a, b);
+	cin.tie(0) -> sync_with_stdio(0);
+	string s = "";
+	while (s != "END")
+	{
+		cin >> s;
+		
+	}
 	return 0;
 }
